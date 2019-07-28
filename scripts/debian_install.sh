d3b6dcfedd3c25c200f8f84f3b858232f77883de
Jianshen Liu <jliu120@ucsc.edu>
2019-07-28 11:09:30 -0700

    Format install script

--- scripts/debian_install.sh
+++ scripts/debian_install.sh
@@ -2,17 +2,17 @@
 
 set -eu -o pipefail
 
-ID_LIKE=$( grep -oP '^ID_LIKE=\K\w+' /etc/os-release 2> /dev/null )
-family=$( echo "$ID_LIKE" | tr '[:upper:]' '[:lower:]' )
+ID_LIKE=$(grep -oP '^ID_LIKE=\K\w+' /etc/os-release 2> /dev/null)
+family=$(echo "$ID_LIKE" | tr '[:upper:]' '[:lower:]')
 if [ "$family" != "debian" ]; then
-    echo "Operation aborted because the current OS is not a Debian-based distribution."
-    exit 0
+  echo "Operation aborted because the current OS is not a Debian-based distribution."
+  exit 0
 fi
 
-jdk_pkgs=( )
+jdk_pkgs=()
 # javac is used for syntastic checker for .java file
-if ! hash javac 2>/dev/null; then
-    jdk_pkgs=( openjdk-8-jdk openjdk-8-jre-headless )
+if ! hash javac 2> /dev/null; then
+  jdk_pkgs=(openjdk-8-jdk openjdk-8-jre-headless)
 fi
 
 # perl for Checkpatch (syntax checking for C)
@@ -31,44 +31,42 @@ fi
 
 ## shellcheck for syntax checking of sh
 sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-        curl \
-        tar \
-        vim-nox \
-        git \
-        perl \
-        g++ \
-        python3-pip \
-        python3-setuptools \
-        python3-wheel \
-        cppcheck \
-        exuberant-ctags \
-        clang-format \
-        python-dev \
-        python3-dev \
-        build-essential \
-        cmake \
-        libffi-dev \
-        libssl-dev \
-        shellcheck \
-        ${jdk_pkgs[@]+"${jdk_pkgs[@]}"} \
-    && sudo apt-get clean \
-    && sudo rm -rf /var/lib/apt/lists/*
-
+  curl \
+  tar \
+  vim-nox \
+  git \
+  perl \
+  g++ \
+  python3-pip \
+  python3-setuptools \
+  python3-wheel \
+  cppcheck \
+  exuberant-ctags \
+  clang-format \
+  python-dev \
+  python3-dev \
+  build-essential \
+  cmake \
+  libffi-dev \
+  libssl-dev \
+  shellcheck \
+  ${jdk_pkgs[@]+"${jdk_pkgs[@]}"} &&
+  sudo apt-get clean &&
+  sudo rm -rf /var/lib/apt/lists/*
 
 # Get the full directory name of the current script
 # See https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Install Vundle and Plugins
 cp "${SCRIPT_DIR}"/../.vimrc "$HOME"
-rm -rf "${HOME}"/.vim/bundle/Vundle.vim && \
-    git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}"/.vim/bundle/Vundle.vim && \
-    vim +PluginClean! +PluginInstall! +qall && \
-    sed -i 's/"#//g' "$HOME"/.vimrc
+rm -rf "${HOME}"/.vim/bundle/Vundle.vim &&
+  git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}"/.vim/bundle/Vundle.vim &&
+  vim +PluginClean! +PluginInstall! +qall &&
+  sed -i 's/"#//g' "$HOME"/.vimrc
 
 # make folder to persistent undo used by plugin mbbill/undotree
 mkdir -p "$HOME"/.undodir
-
 
 # Remove the old exported envs first
 sed -i '/ljishen\/my-vim/,/#### END ####/d' "$HOME"/.profile
@@ -79,11 +77,11 @@ sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "$HOME"/.profile
 
 printf "\\n\\n#### Export Variables for Vim Plugins (https://github.com/ljishen/my-vim) ####\\n\\n" >> "$HOME"/.profile
 
-function export_envs {
-    for env in $1; do
-        export "${env?}"
-        printf "export %s\\n" "$env" >> "$HOME"/.profile
-    done
+function export_envs() {
+  for env in $1; do
+    export "${env?}"
+    printf "export %s\\n" "$env" >> "$HOME"/.profile
+  done
 }
 
 # Check if TERM is already set so that it is not overwritten if tmux/screen
@@ -92,7 +90,6 @@ update_term="if [ -z \"\$TERM\" ]; then export TERM=xterm-256color; fi"
 eval "$update_term"
 printf "%s\\n" "$update_term" >> "$HOME"/.profile
 
-
 # Install js-beautify as the JSON Formatter for plugin google/vim-codefmt
 # Install bandit, flake8, pycodestyle and pydocstyle as the syntax checkers
 #     for Python3 used in plugin vim-syntastic/syntastic
@@ -100,21 +97,20 @@ printf "%s\\n" "$update_term" >> "$HOME"/.profile
 # pylint is a code linter for Python used by plugin vim-syntastic/syntastic
 # ansible-lint is a best-practices linter for Ansible playbooks used by plugin vim-syntastic/syntastic
 pip3 install --upgrade \
-    jsbeautifier \
-    flake8 \
-    mypy \
-    bandit \
-    pylint \
-    pycodestyle \
-    pydocstyle \
-    ansible-lint
+  jsbeautifier \
+  flake8 \
+  mypy \
+  bandit \
+  pylint \
+  pycodestyle \
+  pydocstyle \
+  ansible-lint
 
 # Compiling YouCompleteMe(YCM) with semantic support for Java and C-family languages (through libclang and clangd)
 "$HOME"/.vim/bundle/YouCompleteMe/install.py \
-    --clang-completer \
-    --clangd-completer \
-    --java-completer
-
+  --clang-completer \
+  --clangd-completer \
+  --java-completer
 
 # Install various checkers for plugin vim-syntastic/syntastic
 
@@ -124,16 +120,16 @@ mkdir -p "$SYNTASTIC_HOME"
 # Install Checkstyle (for Java)
 CHECKSTYLE_VERSION=8.20
 CHECKSTYLE_HOME="${SYNTASTIC_HOME}"/checkstyle
-mkdir -p "${CHECKSTYLE_HOME}" && \
-    curl -fsSL https://github.com/checkstyle/checkstyle/releases/download/checkstyle-"${CHECKSTYLE_VERSION}"/checkstyle-"${CHECKSTYLE_VERSION}"-all.jar -o "${CHECKSTYLE_HOME}"/checkstyle-"${CHECKSTYLE_VERSION}"-all.jar
+mkdir -p "${CHECKSTYLE_HOME}" &&
+  curl -fsSL https://github.com/checkstyle/checkstyle/releases/download/checkstyle-"${CHECKSTYLE_VERSION}"/checkstyle-"${CHECKSTYLE_VERSION}"-all.jar -o "${CHECKSTYLE_HOME}"/checkstyle-"${CHECKSTYLE_VERSION}"-all.jar
 curl -fsSL https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml -o "${CHECKSTYLE_HOME}"/google_checks.xml
 export_envs "CHECKSTYLE_JAR=${CHECKSTYLE_HOME}/checkstyle-${CHECKSTYLE_VERSION}-all.jar \
              CHECKSTYLE_CONFIG=${CHECKSTYLE_HOME}/google_checks.xml"
 
 # Install Checkpatch
 CHECKPATCH_HOME="${SYNTASTIC_HOME}"/checkpatch
-mkdir -p "${CHECKPATCH_HOME}" && \
-    curl -fsSL https://raw.githubusercontent.com/torvalds/linux/master/scripts/checkpatch.pl -o "${CHECKPATCH_HOME}"/checkpatch.pl
+mkdir -p "${CHECKPATCH_HOME}" &&
+  curl -fsSL https://raw.githubusercontent.com/torvalds/linux/master/scripts/checkpatch.pl -o "${CHECKPATCH_HOME}"/checkpatch.pl
 chmod +x "${CHECKPATCH_HOME}"/checkpatch.pl
 PATH="${CHECKPATCH_HOME}:$PATH"
 
@@ -141,24 +137,24 @@ PATH="${CHECKPATCH_HOME}:$PATH"
 GOOGLE_JAVA_FORMAT_VERSION=1.7
 GOOGLE_JAVA_FORMAT_HOME="${SYNTASTIC_HOME}"/google-java-format
 export_envs "GOOGLE_JAVA_FORMAT_JAR=${GOOGLE_JAVA_FORMAT_HOME}/google-java-format-${GOOGLE_JAVA_FORMAT_VERSION}-all-deps.jar"
-mkdir -p "${GOOGLE_JAVA_FORMAT_HOME}" && \
-    curl -fsSL https://github.com/google/google-java-format/releases/download/google-java-format-"${GOOGLE_JAVA_FORMAT_VERSION}"/google-java-format-"${GOOGLE_JAVA_FORMAT_VERSION}"-all-deps.jar -o "${GOOGLE_JAVA_FORMAT_JAR}"
+mkdir -p "${GOOGLE_JAVA_FORMAT_HOME}" &&
+  curl -fsSL https://github.com/google/google-java-format/releases/download/google-java-format-"${GOOGLE_JAVA_FORMAT_VERSION}"/google-java-format-"${GOOGLE_JAVA_FORMAT_VERSION}"-all-deps.jar -o "${GOOGLE_JAVA_FORMAT_JAR}"
 
 # Install hadolint (for Dockerfile)
-if [[ $(arch) = x86_64 ]]; then
-    HADOLINT_VERSION=1.16.3
-    HADOLINT_HOME="${SYNTASTIC_HOME}"/hadolint
-    mkdir -p "${HADOLINT_HOME}" && \
-        curl -fsSL https://github.com/hadolint/hadolint/releases/download/v"${HADOLINT_VERSION}"/hadolint-Linux-x86_64 -o "${HADOLINT_HOME}"/hadolint
-    chmod +x "${HADOLINT_HOME}"/hadolint
-    PATH="${HADOLINT_HOME}:$PATH"
+if [[ $(arch) == x86_64 ]]; then
+  HADOLINT_VERSION=1.16.3
+  HADOLINT_HOME="${SYNTASTIC_HOME}"/hadolint
+  mkdir -p "${HADOLINT_HOME}" &&
+    curl -fsSL https://github.com/hadolint/hadolint/releases/download/v"${HADOLINT_VERSION}"/hadolint-Linux-x86_64 -o "${HADOLINT_HOME}"/hadolint
+  chmod +x "${HADOLINT_HOME}"/hadolint
+  PATH="${HADOLINT_HOME}:$PATH"
 fi
 
 # Because mypy is installed to the "$HOME"/.local/bin,
 #     we need to add it to the PATH if it doesn't already exist
 home_local_bin="$HOME"/.local/bin
-if [[ :$PATH: != *:"$home_local_bin":* ]] ; then
-    PATH="${home_local_bin}:$PATH"
+if [[ :$PATH: != *:"$home_local_bin":* ]]; then
+  PATH="${home_local_bin}:$PATH"
 fi
 
 # Install Bear to support C-family semantic completion used by YouCompleteMe
@@ -181,8 +177,8 @@ printf "\\n#### END ####" >> "$HOME"/.profile
 
 # Config .tmux.conf for Vim running inside tmux
 tmux_conf_file="$HOME"/.tmux.conf
-if [ ! -f  "$tmux_conf_file" ] || ! grep -q "tmux-256color" "$tmux_conf_file"; then
-    echo 'set -g default-terminal "tmux-256color"' >> "$tmux_conf_file"
+if [ ! -f "$tmux_conf_file" ] || ! grep -q "tmux-256color" "$tmux_conf_file"; then
+  echo 'set -g default-terminal "tmux-256color"' >> "$tmux_conf_file"
 fi
 
 printf "\\nInstallation completed successfully.\\n"
